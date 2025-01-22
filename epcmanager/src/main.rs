14906615aaf11038f718959d89dfd57bf5ee65fe
Yasuyuki Nakamura <ynakamura@forevision.co.jp>
2025-01-22 15:46:54 +0900

    textbox

--- epcmanager/src/main.rs
+++ epcmanager/src/main.rs
@@ -1,5 +1,4 @@
-use iced::widget::{button, text,column,combo_box};
-use iced::Element;
+use iced::widget::{button, text_input,column,combo_box};
 use iced::Fill;
 
 #[derive(Default,Clone,Copy, PartialEq, Eq,Debug)]
@@ -27,24 +26,30 @@ impl std::fmt::Display for Bit {
 }
 
 
+
+
 struct EpcManager {
-    epcs: Vec<String>,
+    ascii_string: String,
+    hex_string: String,
     bit: combo_box::State<Bit>,
     selected_bit: Option<Bit>,
 
 }
 
-#[derive(Debug, Clone,Copy)]
+#[derive(Debug, Clone)]
 enum Message {
     Encode,
     Decode,
     SelectedBit(Bit),
+    AsciiChanged(String),
+    HexChanged(String),
 }
 
 impl EpcManager {
     fn new() -> Self {
         Self {
-            epcs: Vec::new(),
+            ascii_string: String::new(),
+            hex_string: String::new(),
             bit: combo_box::State::new(Bit::ALL.to_vec()),
             selected_bit: Bit::Eight.into(),
         }
@@ -52,13 +57,19 @@ impl EpcManager {
     fn update(&mut self, message: Message) {
         match message {
             Message::Encode => {
-                self.epcs.push("Hello".to_string());
+                self.hex_string = self.ascii_string.clone();
             }
             Message::Decode => {
-                self.epcs.pop();
+                self.ascii_string = self.hex_string.clone();
             }
             Message::SelectedBit(bit) => {
                 self.selected_bit = Some(bit);
+            }
+            Message::AsciiChanged(ascii) => {
+                self.ascii_string = ascii;
+            }
+            Message::HexChanged(hex) => {
+                self.hex_string = hex;
             }
         
         }
@@ -69,6 +80,8 @@ impl EpcManager {
             button("Encode").on_press(Message::Encode),
             combo_box(&self.bit, "bit", self.selected_bit.as_ref(),Message::SelectedBit)
             ,
+            text_input("ASCII", &self.ascii_string).on_input(Message::AsciiChanged),
+            text_input("HEX", &self.hex_string).on_input(Message::HexChanged),
             button("Decode").on_press(Message::Decode),
         ].width(Fill).spacing(10)
             .into()
